c93172bab73fb49ca82ed97f76463039427fbc8b
innlouvate <l.j.franklin85@googlemail.com>
2016-01-25 16:26:10 +0000

    top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,11 +4,16 @@ describe Oystercard do
   subject(:oystercard) {described_class.new}
 
   describe "#balance" do
-
     it "is initialised with a balance of 0 by default" do
       expect(oystercard.balance).to eq 0
     end
+  end
 
+  describe '::top_up' do
+    it 'adds the value specified to the balance' do
+      value = rand(100)
+      expect{oystercard.top_up(value)}. to change{ oystercard.balance }.from(0).to(value)
+    end
   end
 
 end
