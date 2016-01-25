b64ac43e87b7040a53cb87343abe587fe4192033
Marion LV <marion.levisage@gmail.com>
2016-01-25 16:13:49 +0000

    Created an oyster card and balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
+require 'oystercard'
+
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
+
+  describe "#balance" do
+
+    it "is initialised with a balance of 0 by default" do
+      expect(oystercard.balance).to eq 0
+    end
+
+  end
 
 end
